ed2ac59876348d9917ed68ea2aacc1dd5f707f9d
Adam Pawliński <adam.pawlinski@gmail.com>
2019-02-18 15:01:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dee693ce9f66dbd1a5b82e54fdd9448.js"
+  "/precache-manifest.3ecaf1de3b20b3faccd3d0b55e0d8acb.js"
 );
 
 workbox.clientsClaim();
